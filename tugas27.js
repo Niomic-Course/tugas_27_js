4e3e96b3c74a47951e5dbc2e5c7588147d415b86
Saddam Husein <saddambinhusein2@gmail.com>
2021-07-10 00:03:49 +0800

    Tugas 27 JS Pengumpulan tugas 27 JS ke Niomic

---
+++ tugas27.js
@@ -0,0 +1,14 @@
+function gabungKata() {
+    // No. 1:
+    let gabung1 = 'No';
+    console.log(gabung1.concat(' rumah saya ', 30, 'A'));
+
+    // No. 2:
+    let gab1 = 'Jalan pangeran Tirtayasa No ';
+    let gab2 = 70;
+    result = gab1 + gab2;
+    console.log(result);
+    
+}
+
+gabungKata();
